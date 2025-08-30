3c9de90e9c0a2df291d9d3eaaf8f837af23798d9
2823558582@qq.com <2823558582@qq.com>
2025-08-30 07:34:04 +0000

    Edited nav; Deleted Form1

--- client_code/nav/__init__.py
+++ client_code/nav/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import navTemplate
 from anvil import *
+import anvil
+import base64
 
 class nav(navTemplate):
     def __init__(self, **properties):
@@ -7,3 +9,27 @@ class nav(navTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    
+    # 选完文件以后会自动触发此事件
+    def file_loader_1_change(self, file, **event_args):
+        if file is None:                 # 用户点取消
+            return
+    
+        # 简单做个类型校验（可选）
+        if file.content_type != 'application/pdf':
+            alert("请上传 pdf 文件")        # 也可以做更完善的判断
+            return
+
+        # 1. 把 Media 对象转成 data URL（base64）
+        b64 = base64.b64encode(file.get_bytes()).decode("ascii")
+        data_url = f"data:application/pdf;base64,{b64}"
+    
+        # 2. 生成 <iframe> 代码
+        iframe_html = (
+            f'<iframe src="{data_url}" '
+            f'width="100%" height="700px" style="border:none;"></iframe>'
+        )
+    
+        # 3. 填进 RichText 组件
+        self.pdf_viewer.content = iframe_html     
