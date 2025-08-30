ad1bda7cc6d43a31b02d17a2262be3855ef24752
2823558582@qq.com <2823558582@qq.com>
2025-08-30 13:38:29 +0000

    Edited a_jianlixiugai, b_fatie, c_news, d_wode and 2 more

--- client_code/nav/__init__.py
+++ client_code/nav/__init__.py
@@ -1,8 +1,8 @@
 from ._anvil_designer import navTemplate
 from anvil import *
-import anvil.facebook.auth
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.users
 
 
