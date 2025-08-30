873b8bdd53b10778dcc7b00b8c52e6dc594dddfa
2823558582@qq.com <2823558582@qq.com>
2025-08-30 13:37:50 +0000

    Edited a_jianlixiugai, b_fatie, c_news, d_wode and 2 more

--- client_code/a_jianlixiugai/__init__.py
+++ client_code/a_jianlixiugai/__init__.py
@@ -3,9 +3,6 @@ from anvil import *
 import anvil.facebook.auth
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import anvil.users
 from anvil import *
 import anvil
--- client_code/nav/__init__.py
+++ client_code/nav/__init__.py
@@ -4,9 +4,6 @@ import anvil.facebook.auth
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
 
 class nav(navTemplate):
