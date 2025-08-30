41cf8caacc392b179087f9d52a93c2f486a319f9
2823558582@qq.com <2823558582@qq.com>
2025-08-30 13:12:36 +0000

    Created b_fatie, d_wode and c_news; Edited settings, nav and a_jianlixiugai; Deleted Snipaste_2025-08-20_10-50-39.png and Form1

--- client_code/c_news/__init__.py
+++ client_code/c_news/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import c_newsTemplate
 from anvil import *
 import anvil.facebook.auth
 import anvil.google.auth, anvil.google.drive
@@ -9,7 +9,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class c_news(c_newsTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
