8ee2df987fda25c0ab4c1985caa350dd18c1a6bd
wjsw1781 <1781591279@qq.com>
2025-09-01 15:23:25 +0800

    sf

--- 启动.sh
+++ 启动.sh
@@ -1,7 +1,8 @@
+数据库删除
+rm -rf /root/pdf/.anvil-data 
 
-
-anvil-app-server --app . --ip 0.0.0.0 --port 3030 
-nohup anvil-app-server --app . --ip 0.0.0.0 --port 3030 >/dev/null 2>&1 &
+anvil-app-server --app . --ip 0.0.0.0 --port 80 
+nohup anvil-app-server --app . --ip 0.0.0.0 --port 80 >/dev/null 2>&1 &
 
 ps aux | grep anvil-app-server 
 ps aux | grep anvil-app-server | awk '{print $2}' | xargs kill -9
