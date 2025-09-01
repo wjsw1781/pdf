d54eff17456ca609c773131d6c6327ca94c97db0
wjsw1781 <1781591279@qq.com>
2025-09-01 21:03:08 +0800

    水电费

--- 启动.sh
+++ 启动.sh
@@ -1,11 +1,17 @@
 
+export http_proxy="socks5h://127.0.0.1:20160"
+export https_proxy="socks5h://127.0.0.1:20160"
+sudo apt install certbot
+sudo certbot certonly --standalone -d l4proxy.top -m 1781591279@qq.com --agree-tos --non-interactive
+sudo certbot renew --dry-run
 
 
 数据库删除
 rm -rf /root/pdf/.anvil-data 
 
-anvil-app-server --app . --ip 0.0.0.0 --port 80 
-nohup anvil-app-server --app . --ip 0.0.0.0 --port 80 >/dev/null 2>&1 &
+anvil-app-server --origin https://l4proxy.top --manual-cert-file /etc/letsencrypt/live/l4proxy.top/fullchain.pem --manual-cert-key-file /etc/letsencrypt/live/l4proxy.top/privkey.pem --port 443
+nohup anvil-app-server --origin https://l4proxy.top --manual-cert-file /etc/letsencrypt/live/l4proxy.top/fullchain.pem --manual-cert-key-file /etc/letsencrypt/live/l4proxy.top/privkey.pem --port 443
+ >/dev/null 2>&1 &
 
 ps aux | grep anvil-app-server 
 ps aux | grep anvil-app-server | awk '{print $2}' | xargs kill -9
@@ -14,15 +20,3 @@ ps aux | grep anvil-app-server | awk '{print $2}' | xargs kill -9
 101.132.61.226
 
 curl 101.132.61.226
-
-stripe密钥
-kzpf-xqow-cnfh-tbso-tutq
-
-
-pk_live_51S2RoqPN2nVEVllyusrxayXURtyYOeD2TxxGvNs99Q6HCWxK0wluq3N2eK7uXvBc61x2K8kz95Vv6APL12vLCEQ900FRH1gmxL
-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
-
-
-
-
-
