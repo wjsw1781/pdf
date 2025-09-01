8d15ee492fb6d1a8d08af2c3e9346a1cb4467f07
wjsw1781 <1781591279@qq.com>
2025-09-01 15:24:30 +0800

    sdf

--- 启动.sh
+++ 启动.sh
@@ -8,7 +8,7 @@ ps aux | grep anvil-app-server
 ps aux | grep anvil-app-server | awk '{print $2}' | xargs kill -9
 
 
-101.132.61.226:3030
+101.132.61.226
 
-curl 101.132.61.226:3030
+curl 101.132.61.226
 
