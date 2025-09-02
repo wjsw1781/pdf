c4fae7ace34646987565850503cea5d2470f9f46
wjsw1781 <1781591279@qq.com>
2025-09-02 11:28:22 +0800

    水电费

--- 启动.sh
+++ 启动.sh
@@ -8,7 +8,9 @@ git config  credential.helper store
 
 sudo apt install certbot
 sudo certbot certonly --standalone -d l4proxy.top -m 1781591279@qq.com --agree-tos --non-interactive
-/etc/letsencrypt/live/l4proxy.top/privkey.pem
+
+Certificate is saved at: /etc/letsencrypt/live/l4proxy.top/fullchain.pem
+Key is saved at:         /etc/letsencrypt/live/l4proxy.top/privkey.pem
 
 sudo certbot renew --dry-run
 
@@ -20,14 +22,11 @@ anvil-app-server --ip 0.0.0.0 --port 8443
 
 anvil-app-server --origin https://l4proxy.top --manual-cert-file /etc/letsencrypt/live/l4proxy.top/fullchain.pem --manual-cert-key-file /etc/letsencrypt/live/l4proxy.top/privkey.pem --port 443
 
-nohup anvil-app-server --origin https://l4proxy.top --manual-cert-file /etc/letsencrypt/live/l4proxy.top/fullchain.pem --manual-cert-key-file /etc/letsencrypt/live/l4proxy.top/privkey.pem --port 443
- >/dev/null 2>&1 &
+nohup anvil-app-server --origin https://l4proxy.top --manual-cert-file /etc/letsencrypt/live/l4proxy.top/fullchain.pem --manual-cert-key-file /etc/letsencrypt/live/l4proxy.top/privkey.pem --port 443 >/dev/null 2>&1 &
 
 ps aux | grep anvil-app-server 
 ps aux | grep anvil-app-server | awk '{print $2}' | xargs kill -9
 
-
-101.132.61.226
 
 curl  http://l4proxy.top 
 
