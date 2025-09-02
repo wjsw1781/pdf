abe364fd03951dce77e0a465410c53a061ff68dd
wjsw1781 <1781591279@qq.com>
2025-09-02 14:24:18 +0800

    撒发的

--- 启动.sh
+++ 启动.sh
@@ -47,7 +47,6 @@ nohup anvil-app-server --origin https://l4proxy.top --manual-cert-file /etc/lets
 
 ps aux | grep anvil-app-server 
 ps aux | grep anvil-app-server | awk '{print $2}' | xargs kill -9  
-ps aux | grep anvil-app-server | awk '{print $2}' | xargs kill -9  &&  nohup anvil-app-server --origin https://l4proxy.top --manual-cert-file /etc/letsencrypt/live/l4proxy.top/fullchain.pem --manual-cert-key-file /etc/letsencrypt/live/l4proxy.top/privkey.pem --port 443 >/dev/null 2>&1 &
 
 
 
