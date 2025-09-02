7356633c03f3b4e434c546bf68f2c51d7d03fb44
2823558582@qq.com <2823558582@qq.com>
2025-09-02 03:27:57 +0000

    Created paddle; Edited settings

---
+++ client_code/paddle/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import paddleTemplate
+from anvil import *
+import anvil.server
+from routing import router
+import stripe.checkout
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class paddle(paddleTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
