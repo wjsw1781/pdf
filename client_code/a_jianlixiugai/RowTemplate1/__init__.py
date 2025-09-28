6a8bba7c9dae977080dade074cbc71956ddede34
2823558582@qq.com <2823558582@qq.com>
2025-09-28 17:05:54 +0000

    Edited a_jianlixiugai.RowTemplate1 and a_jianlixiugai

--- client_code/a_jianlixiugai/RowTemplate1/__init__.py
+++ client_code/a_jianlixiugai/RowTemplate1/__init__.py
@@ -10,6 +10,7 @@ class RowTemplate1(RowTemplate1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        
 
 
     def down_item_click(self, **event_args):
