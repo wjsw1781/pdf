c1e33b2a60bc7eb56356c5f28386b2fdf72370ee
2823558582@qq.com <2823558582@qq.com>
2025-09-28 17:31:35 +0000

    Created d_wode.RowTemplate8; Edited d_wode and nav; Deleted a_jianlixiugai.RowTemplate1

--- client_code/d_wode/RowTemplate8/__init__.py
+++ client_code/d_wode/RowTemplate8/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate1Template
+from ._anvil_designer import RowTemplate8Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -6,11 +6,14 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class RowTemplate1(RowTemplate1Template):
+
+class RowTemplate8(RowTemplate8Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        
+
+        # Any code you write here will run before the form opens.
+
 
 
     def button_2_click(self, **event_args):
